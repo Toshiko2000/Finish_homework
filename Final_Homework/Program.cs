f0fdd9087b613ffc299da0354e1434099c75f969
Toshiko2000 <118428342+Toshiko2000@users.noreply.github.com>
2022-12-28 22:42:36 +0300

    Написал программу

--- Final_Homework/Program.cs
+++ Final_Homework/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿            Console.Clear();
+            Console.Write("Сколько элементов Вы хотите ввести? ");
+
+            int size = Convert.ToInt32(Console.ReadLine());
+            string[] arrayStrings = new string[size];
+            int pos = 0; 
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Введите {i+1}-й элемент: ");
+                string element = Convert.ToString(Console.ReadLine()); 
+                if (element.Length <= 3) 
+                {
+                    arrayStrings[pos++] = element; 
+                }                                                                                          
+            }
+            
+
+            Console.WriteLine(); 
+            PrintArray(arrayStrings); 
+
+            void PrintArray(string[] array) 
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write(array[i] + " ");
+                }
+                Console.WriteLine();
+            }
+        
+    
